9dd6b4f92dcf4983454124f8620842f5868d6f04
Lalk4 <57485454+Lalk4@users.noreply.github.com>
2021-01-21 15:12:59 +0500

    Work with products

---
+++ src/components/ProductList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem v-for="product in products" :product="product" :key="product.title"/>
+  </ul>
+</template>
+
+<script>
+import ProductItem from "@/components/ProductItem";
+
+export default {
+  props: ['products'],
+  components: {ProductItem},
+  name: "ProductList",
+  data: () => ({
+
+  })
+}
+</script>
